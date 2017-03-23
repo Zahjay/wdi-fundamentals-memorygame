206e28100a7fc870598f1170cb233ebad1af2006
user <user@users-mbp.wifi.lagcc>
2017-03-23 09:31:40 -0400

    adding an alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,12 @@ var cardTwo = "king";
 var cardThree= "queen";
 var cardFour = "king";
 
+if (cardTwo === cardFour){
+alert("You’ve found a match!");
+ } else(cardOne != cardTwo)
+{
+  alert("Sorry,try again.");
+}  
+             
+
+
