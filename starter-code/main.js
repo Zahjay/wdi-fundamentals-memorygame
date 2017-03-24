37921365cc69c4d1ba0253cb405e1df0f03f65fe
user <user@users-mbp.wifi.lagcc>
2017-03-24 10:03:47 -0400

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,33 @@
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "king";
 var cardThree= "queen";
-var cardFour = "king";
+var cardFour = "king";*/
 
-if (cardTwo === cardFour){
+//This is adding the array
+var cards = ["queen", "king", "queen", "king"] ;
+//variable with 
+var gameBoard =  document.getElementById('game-board');
+ 
+function createCards() { 
+ 
+ for(var i = 0; i< cards.length; i++) {
+ 	var cardOne = document.createElement("div");
+ 	cardOne.classList.add("card");
+ 	gameBoard.appendChild(cardOne)
+ }
+
+ 
+}
+
+ // var createCards = ;
+ createCards();
+
+/*if (cardTwo === cardFour){
 alert("You’ve found a match!");
  } else(cardOne != cardTwo)
 {
   alert("Sorry,try again.");
-}  
+} */ 
              
 
 
