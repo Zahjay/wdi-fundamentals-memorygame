af82dcd149a945996a5a46d21fbebee652e3442f
user <user@users-mbp.wifi.lagcc>
2017-03-24 16:06:50 -0400

    Unit 11 Completed Memory Game

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,30 +4,60 @@ var cardThree= "queen";
 var cardFour = "king";*/
 
 //This is adding the array
+var cardsInPlay  = [];
+
 var cards = ["queen", "king", "queen", "king"] ;
 //variable with 
 var gameBoard =  document.getElementById('game-board');
  
 function createCards() { 
  
- for(var i = 0; i< cards.length; i++) {
- 	var cardOne = document.createElement("div");
- 	cardOne.classList.add("card");
- 	gameBoard.appendChild(cardOne)
+	 for(var i = 0; i < cards.length; i++) {
+	 	var cardOne = document.createElement("div");
+
+	 	cardOne.setAttribute('data-card', cards[i]);
+	 	cardOne.classList.add("card");
+	 	cardOne.addEventListener('click',isTwoCards);
+	 	//cardOne.innerHTML = '<img src="images/kingForGame.jpg" alt="King Picture">';
+	 	gameBoard.appendChild(cardOne)
+	 	
+	 }
  }
 
- 
+function isTwoCards(){
+ 	//cardsInPlay.push(this.getAttribute('data-card'));
+ 	cardsInPlay.push(this);
+ 	if(this.getAttribute("data-card") === "queen"){
+ 		this.innerHTML = '<img src="images/queenForGame.jpg" alt="Queen Picture">';
+ 	} 
+ 	if(this.getAttribute("data-card") === "king") {
+ 		this.innerHTML = '<img src="images/kingForGame.jpg" alt="Queen Picture">';
+ 	}
+ 	
+ 	if (cardsInPlay.length === 2) {
+    	isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
 }
 
- // var createCards = ;
+function isMatch(cardsInPlay){
+	console.log(cardsInPlay);
+
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You’ve found a match!");
+ 	}
+ 	 else {
+	  	alert("Sorry,try again.");
+	} 
+
+	cardsInPlay[0].innerHTML = "";
+	cardsInPlay[1].innerHTML = "";
+   // if (this.getAttribute('data-card') === "queen"){
+   // 	this.innerHTML = '<img src="images/queenForGame.jpg" alt="Queen Picture">';
+   // }
+
+
+	//console.log(cardsInPlay);
+  }
+
  createCards();
-
-/*if (cardTwo === cardFour){
-alert("You’ve found a match!");
- } else(cardOne != cardTwo)
-{
-  alert("Sorry,try again.");
-} */ 
-             
-
-
